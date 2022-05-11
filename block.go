8a1d91f9015b83c5998f982976d7243c69c3c8b5
Hao Wang <58178768+wanghao-create@users.noreply.github.com>
2022-05-11 08:30:44 +0800

    block.go

---
+++ block.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Transactions  []*Transaction
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+	Height        int
+}
+
+func NewBlock(transactions []*Transaction, prevBlockHash []byte, height int) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0, height}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
