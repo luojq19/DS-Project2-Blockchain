b2061af5a84b1e750c18a1c566a4e22e0a409378
Hao Wang <58178768+wanghao-create@users.noreply.github.com>
2022-05-11 10:26:30 +0800

    Add variadic parameter

--- blockchain.go
+++ blockchain.go
@@ -6,13 +6,26 @@ type Blockchain struct {
 }
 
 // create a new bock
-func NewBlockchain(difficulty int64) *Blockchain {
+func NewBlockchain(difficultyList ...int64) *Blockchain {
+	var difficulty int64
+	difficulty = 24
+	for _, num := range difficultyList {
+		difficulty = num
+	}
 	return &Blockchain{[]*Block{NewGenesisBlock(difficulty)}}
 }
 
 // add a new block to the end of blockchain
-func (bc *Blockchain) AddBlock(data string, difficulty int64) {
+func (bc *Blockchain) AddBlock(data string, difficultyList ...int64) {
+	var difficulty int64
+
 	prevBlock := bc.blocks[len(bc.blocks)-1]
+
+	difficulty = prevBlock.Difficulty
+	for _, num := range difficultyList {
+		difficulty = num
+	}
+
 	newBlock := NewBlock(data, prevBlock.Hash, difficulty)
 	bc.blocks = append(bc.blocks, newBlock)
 }
--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-const difficulty = 24
+const difficulty = 16
 
 func main() {
 	bc := NewBlockchain(difficulty)
 	bc.AddBlock("Send 1 BTC to Ivan", difficulty)
-	bc.AddBlock("Send 2 more BTC to Ivan", difficulty)
+	bc.AddBlock("Send 2 more BTC to Ivan")
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
